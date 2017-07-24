f48cf3ebbebe9bbbad268465207b7084fc2fcaa6
CuiyeCLJ <1501198672@qq.com>
2017-07-24 11:03:39 +0800

    11:03 2017.07.24

--- app/src/main/java/android/bemodel/com/bemodel/view/ModelCircleActivity.java
+++ app/src/main/java/android/bemodel/com/bemodel/view/ModelCircleActivity.java
@@ -2,6 +2,7 @@ package android.bemodel.com.bemodel.view;
 
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.bemodel.com.bemodel.R;
@@ -16,7 +17,9 @@ public class ModelCircleActivity extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         rootView = inflater.inflate(R.layout.activity_model_circle, container, false);
+        
 
         return rootView;
     }
